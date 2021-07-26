77c2512cee160128c75cab5bc61d08ac833d6df4
RDDcat <yukmaro@gmail.com>
2021-07-26 17:42:29 +0900

    [ADD] member domain created
    멤버 도메인 만들었습니다.

--- src/main/java/first/helospring/repository/MemberRepository.java
+++ src/main/java/first/helospring/repository/MemberRepository.java
@@ -1,2 +1,14 @@
-package first.helospring.repository;public interface MemberRepository {
+package first.helospring.repository;
+
+import first.helospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
 }
