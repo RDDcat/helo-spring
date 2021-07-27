9359ae5fc28cc021027ac2490308febcf5873ae1
RDDcat <yukmaro@gmail.com>
2021-07-27 17:59:08 +0900

    [ADD] JPA member repository
    JPA로 만든 멤버서비스 & 리포지토리

--- src/main/java/first/helospring/SpringConfig.java
+++ src/main/java/first/helospring/SpringConfig.java
@@ -1,24 +1,22 @@
 package first.helospring;
 
-import first.helospring.repository.JdbcMemberRepository;
-import first.helospring.repository.JdbcTemplateMemberRepository;
-import first.helospring.repository.MemberRepository;
-import first.helospring.repository.MemoryMemberRepository;
+import first.helospring.repository.*;
 import first.helospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,7 +28,8 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
--- src/main/java/first/helospring/domain/Member.java
+++ src/main/java/first/helospring/domain/Member.java
@@ -1,8 +1,15 @@
 package first.helospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 데이터를 구분하기위해 시스템이 저장하는 id 값 (내부)
+
+//    @Column(name = "username")
     private String name;
 
     public String getName() {
