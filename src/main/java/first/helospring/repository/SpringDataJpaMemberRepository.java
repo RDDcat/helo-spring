3994567d8c60906b6d52ed8df58051fbbc3889ba
RDDcat <yukmaro@gmail.com>
2021-07-28 09:56:40 +0900

    [ADD] Spring data jpa member repository
    wow

---
+++ src/main/java/first/helospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package first.helospring.repository;
+
+import first.helospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
